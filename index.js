980506a997cadb0074f488120c8f861bb3713659
xJaimeGx <beanieman019@protonmail.com>
2022-04-17 14:24:16 -0500

    add engineer and intern questions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require('inquirer');
-const fs = rquire('fs');
+const fs = require('fs');
 const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
 
 const managerQuestions = () => {
     inquirer.prompt([
@@ -38,4 +40,77 @@ const managerQuestions = () => {
     
 };
 
+const engineerQuestions = () => {
+    inquirer.prompt([
+        {
+            name: 'name',
+            type: 'input',
+            message: 'What is the name of the engineer?',
+        },
+
+        {
+            name: 'id',
+            type: 'input',
+            message: 'What is id of the engineer?',
+        },
+
+        {
+            name: 'email',
+            type: 'input',
+            message: 'What is the email address of the engineer?',
+        },
+
+        {
+            name: 'github',
+            type: 'input',
+            message: 'What is the github username of the engineer?',
+        },
+
+        {
+            name: 'newMember',
+            type: 'list',
+            message: 'What is the title of the new member?',
+            choices: ['Engineer', 'Intern', 'I will not be adding any members'],
+        }
+    ])
+    
+};
+
+const internQuestions = () => {
+    inquirer.prompt([
+        {
+            name: 'name',
+            type: 'input',
+            message: 'What is the name of the intern?',
+        },
+
+        {
+            name: 'id',
+            type: 'input',
+            message: 'What is id of the intern?',
+        },
+
+        {
+            name: 'email',
+            type: 'input',
+            message: 'What is the email address of the intern?',
+        },
+
+        {
+            name: 'school',
+            type: 'input',
+            message: 'What school did the intern attend?',
+        },
+
+        {
+            name: 'newMember',
+            type: 'list',
+            message: 'What is the title of the new member?',
+            choices: ['Engineer', 'Intern', 'I will not be adding any members'],
+        }
+    ])
+    
+};
+
 managerQuestions();
+
